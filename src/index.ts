5fde65b9ae060e838404a382e98b8f1c55ffd574
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-03-19 22:01:01 +0100

    ✨ feat(api): make server variable

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,39 @@
-﻿import Koa from 'koa';
+﻿import Koa, { Next } from 'koa';
 import Router from 'koa-router';
 
 import KoaLogger from 'koa-logger';
 import json from 'koa-json';
 import bodyParser from 'koa-bodyparser';
 
+const PORT = process.env.PORT || 3000;
 const app: Koa = new Koa();
 const router: Router = new Router();
 const logger = KoaLogger;
 
 // Endpoints
-router.get('/', async (ctx, next) => {
-    ctx.body = { message: 'Make Todo Rest Api', isDone: true };
+router.get('/', async (ctx, next: Next): Promise<void> => {
+    ctx.body = {
+        message: 'Make Todo Rest Api',
+        isDone: true
+    };
 
     await next();
 });
 
-router.post("/", async (ctx, next) => {
+router.post('/', async (ctx, next: Next): Promise<void> => {
     ctx.body = ctx.request.body;
     await next();
-})
+});
 
 // Middlewares
 app.use(json());
 app.use(logger());
-app.use(bodyParser())
+app.use(bodyParser());
 
 // Routes
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-app.listen(3000, () => {
-    console.log('Koa running on 3000...');
-});
+const server = app
+    .listen(PORT, () => console.log(`Koa server listening on port: ${3000}`))
+    .on('error', (err: Error) => console.error(err));
