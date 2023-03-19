d4618997e8681b48e207efcc6eb31c166aea0a30
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-03-19 21:39:37 +0100

    ✨ feat(api, endpoints): define get and post endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
-﻿export function logger(message: string): void {
-    console.log(message);
-}
+﻿import Koa from 'koa';
+import Router from 'koa-router';
+
+import KoaLogger from 'koa-logger';
+import json from 'koa-json';
+import bodyParser from 'koa-bodyparser';
+
+const app: Koa = new Koa();
+const router: Router = new Router();
+const logger = KoaLogger;
+
+// Endpoints
+router.get('/', async (ctx, next) => {
+    ctx.body = { message: 'Make Todo Rest Api', isDone: true };
+
+    await next();
+});
+
+router.post("/", async (ctx, next) => {
+    ctx.body = ctx.request.body;
+    await next();
+})
+
+// Middlewares
+app.use(json());
+app.use(logger());
+app.use(bodyParser())
+
+// Routes
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+    console.log('Koa running on 3000...');
+});
